3c751b057b4257bd112678e522cef806560b0023
AWildTeriyaki <terrancexia321@gmail.com>
2025-01-03 17:02:14 -0500

    added logo to navbar

--- src/components/NavbarLocal/NavbarLocal.js
+++ src/components/NavbarLocal/NavbarLocal.js
@@ -4,11 +4,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import logo from '../../assets/logo.png'
+
 function Navbar_Local() {
     return (
       <Navbar fixed="top" expand="md" className="bg-body-tertiary" id="navbar">
       <Container>
-        <Navbar.Brand href="#home">Leaf to Ladle</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            alt="Leaf to Ladle"
+            src={ logo }
+            className="d-inline-block align-top"
+            id="navbar-logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
