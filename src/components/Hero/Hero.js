d15d8e5228ee88b593c6fa4d46d4d66c8e924c5c
AWildTeriyaki <terrancexia321@gmail.com>
2025-01-03 15:01:09 -0500

    Fixed more spacing with cards and breadcrumb. Still dislike cards component but will fix later

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ function Hero(props) {
             <div className="row align-items-center">
                 <div className="col-md-6 d-flex justify-content-center align-items-center" id="test">
                     <div className="jumbotron rounded" id="jumbotron">
-                        <h1 className="display-4 big">100% Vegan, 100% Farm to Table</h1>
+                        <h1 className="display-4 big">100% Vegetarian.<br></br>100% Farm to Table.</h1>
                         <p className="lead small">A plant-based dining experience: Delicious, nourishing, and sustainable.</p>
                     </div>
                 </div>
