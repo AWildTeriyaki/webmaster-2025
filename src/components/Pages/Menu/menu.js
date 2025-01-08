cf88fe66629fcc75c2b3950f62d9b2b28c9a85f4
AWildTeriyaki <terrancexia321@gmail.com>
2025-01-07 21:52:41 -0500

    imported winter menu into project

--- src/components/Pages/Menu/menu.js
+++ src/components/Pages/Menu/menu.js
@@ -1,8 +1,12 @@
 import './menu.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+import winter_menu from '../../../assets/menus/winter_menu.pdf';
+
 const Menu = () => (
-    <h1>Menu!</h1>
+    <div className="menu">
+        <a href={ winter_menu } target="_blank">click here!</a>
+    </div>
 );
 
 export default Menu;
