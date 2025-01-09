c7ef48b821256c79c935a1b9e616e7b94ad77548
AWildTeriyaki <terrancexia321@gmail.com>
2025-01-09 13:12:39 -0500

    Added summer menu

--- src/components/Pages/Menu/menu.js
+++ src/components/Pages/Menu/menu.js
@@ -2,6 +2,7 @@ import './menu.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import spring_menu from '../../../assets/menus/spring_menu.pdf';
+import summer_menu from '../../../assets/menus/summer_menu.pdf';
 import autumn_menu from '../../../assets/menus/autumn_menu.pdf';
 import winter_menu from '../../../assets/menus/winter_menu.pdf';
 
@@ -12,7 +13,7 @@ const Menu = () => (
                 <a href={spring_menu} target="_blank" rel="noreferrer"><h1>Spring</h1></a>
             </div>
             <div className="col" id="summer">
-                <a href={autumn_menu} target="_blank" rel="noreferrer"><h1>Summer</h1></a>
+                <a href={summer_menu} target="_blank" rel="noreferrer"><h1>Summer</h1></a>
             </div>
             <div className="col" id="autumn">
                 <a href={autumn_menu} target="_blank" rel="noreferrer"><h1>Autumn</h1></a>
