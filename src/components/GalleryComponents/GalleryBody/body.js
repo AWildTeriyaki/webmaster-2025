f87502c8f6aff1becfc6a4e79f740abae734d6ff
AWildTeriyaki <terrancexia321@gmail.com>
2025-04-16 22:11:23 -0400

    cool gallery transitions i guess

--- src/components/GalleryComponents/GalleryBody/body.js
+++ src/components/GalleryComponents/GalleryBody/body.js
@@ -1,8 +1,27 @@
 import './body.css';
-
 import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Body() {
+    useEffect(() => {
+        gsap.utils.toArray('img').forEach(img => {
+            gsap.from(img, {
+                opacity: 0,
+                y: 50,
+                duration: 1,
+                scrollTrigger: {
+                    trigger: img,
+                    start: 'top 80%',
+                    toggleActions: 'play none none none',
+                }
+            });
+        });
+    }, []);
+
     return (
         <div className="gallery-body">
             <div className="row gallery-row">
@@ -21,7 +40,7 @@ function Body() {
                 <div className="col gallery-col" id="col3">
                     <img className="long-image" src="https://images.pexels.com/photos/6631965/pexels-photo-6631965.jpeg" alt="Lemon Cake" />
                     <img className="short-image" src="https://images.pexels.com/photos/3185509/pexels-photo-3185509.png" alt="Cherry Cheesecake" />
-                    <img className="long-image" src="https://yogaofcooking.co/wp-content/uploads/2019/03/matcha-panna-cotta-3.jpg" />
+                    <img className="long-image" src="https://yogaofcooking.co/wp-content/uploads/2019/03/matcha-panna-cotta-3.jpg" alt="Matcha Panna Cotta" />
                     <img className="short-image" src="https://www.dashofjazz.com/wp-content/uploads/2023/03/Dash-of-Jazz-Matcha-Mint-Iced-Tea-6.jpg" alt="Matcha Mint Tea" />
                 </div>
             </div>
